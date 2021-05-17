91bcfb0fa25436f36896d270dbdd32e7d03fcc71
Golubev Vladimir <goso@list.ru>
2021-05-17 10:46:48 +0300

    add bonus to println

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ public class Main {
     // Формула следующая: за каждые 20 рублей, потраченные на билет, начисляется 1 миля. Дробные мили не начисляются.
 
     public static void main(String[] args) {
+        int BONUS_STEP = 20;
+
         int price = 23715;
-        int step = 20;
-        int bonus = price / step;
-        System.out.println(bonus);
+        int bonus = price / BONUS_STEP;
+
+        System.out.println("bonus:" + bonus);
     }
 }
